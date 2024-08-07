ab6e97fa268cccdae437c9c53d9bb214865f840e
user.emial <emil.mehraliyev003@gmail.com>
2024-08-07 17:31:55 +0300

    HW completed by changing TreeMap in MapObjectImplementation to ConcurentHashMap

---
+++ src/main/java/telran/CompanyRobot.java
@@ -0,0 +1,39 @@
+package telran;
+
+import telran.employees.Employee;
+import telran.net.*;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CompanyRobot {
+	static AtomicInteger employeeId = new AtomicInteger(1000000);
+	static int N_THREADS = 10;
+	static int N_RUNS = 10000;
+
+	public static void main(String[] args) {
+		TcpClient [] clients = new TcpClient[N_THREADS];
+		startClients(clients);
+		
+
+	}
+
+	private static void startClients(TcpClient[] clients) {
+		for (int i = 0; i < clients.length; i++) {
+			clients[i] = new TcpClient("localhost", 5000);
+			TcpClient client = clients[i];
+			new Thread(() -> {
+				for(int j = 0; j < N_RUNS; j++) {
+					System.out.println("Sending request " + (j + 1) + " from client " + Thread.currentThread().getId());
+					client.sendAndReceive(new Request("addEmployee",
+							new Employee(employeeId.getAndIncrement(),
+									10000, "Dep").getJSON()));
+				}System.out.println("Closing client " + Thread.currentThread().getId());
+				client.close();
+			}).start();
+		}
+		
+	}
+
+}
